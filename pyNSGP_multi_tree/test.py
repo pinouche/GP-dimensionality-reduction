49799be92a8631004004c576119977d54d22a027
marcovirgolin <marco.virgolin@gmail.com>
2021-02-11 21:26:10 +0100

    added example for Thomas at the end of test.py to explain how to use linear scaling on multi output

--- pyNSGP_multi_tree/test.py
+++ pyNSGP_multi_tree/test.py
@@ -29,9 +29,10 @@ X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.5, random
 
 
 # Prepare NSGP settings
-nsgp = NSGP(pop_size=100, max_generations=100, verbose=True, max_tree_size=32, 
+use_linear_scaling = True
+nsgp = NSGP(pop_size=100, max_generations=10, verbose=True, max_tree_size=32, 
 	crossover_rate=0.334, mutation_rate=0.333, op_mutation_rate=0.333, min_depth=2, initialization_max_tree_height=6, 
-	tournament_size=2, use_linear_scaling=True, use_erc=True, use_interpretability_model=True,
+	tournament_size=2, use_linear_scaling=use_linear_scaling, use_erc=True, use_interpretability_model=True,
   use_multi_tree=use_multi_tree, num_sub_functions=int(np.sqrt(X_train.shape[1])+1), # idk, hyper-parameter
 	functions = [AddNode(), SubNode(), MulNode(), DivNode()])
 
@@ -44,3 +45,20 @@ print('len front:',len(front))
 for solution in front:
   print(solution.GetHumanExpression())
   break
+
+
+# example to get the output of a multitree individual
+if use_multi_tree:
+	# i get a random solution
+	solution = front[0]
+	# i get its multi-output
+	# the output is of shape (X_train.shape[0], y.shape[1]) (i.e, n*k)
+	output = solution.GetOutput(X_train)
+	# so, the output of the i-th (1st) head is:
+	i = 1
+	ith_output = output[:,i]
+	# and if you used linear scaling during training, 
+	# do not forget to scale the output using the cached coefficients!
+	if use_linear_scaling:
+		scaled_ith_output = solution.sup_functions[i].ls_a + solution.sup_functions[i].ls_b * ith_output
+		print(scaled_ith_output)
