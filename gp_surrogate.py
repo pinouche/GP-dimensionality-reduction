66cecb8508ec2a0f97ed8d6d9b6f3c0371a0a92a
Thomas <26279201+pinouche@users.noreply.github.com>
2021-03-03 09:34:02 -0500

    fir stack bug

--- gp_surrogate.py
+++ gp_surrogate.py
@@ -22,9 +22,16 @@ def multi_tree_gp_surrogate_model(data_x, low_dim_x, test_data_x, test_data_y, s
             building_blocks_train, building_blocks_test = get_building_blocks(data_x, low_dim_x, test_data_x, num_of_blocks,
                                                                               use_interpretability_model, use_manifold_fitness)
             print(building_blocks_train.shape, building_blocks_test.shape)
-            data_x = np.hstack((data_x, building_blocks_train))
-            test_data_x = np.hstack((test_data_x, building_blocks_test))
-            num_of_blocks += building_blocks_train.shape[1]
+
+            if len(building_blocks_train.shape) == 3:
+                for index in range(building_blocks_train.shape[0]):
+                    data_x = np.hstack((data_x, building_blocks_train[index]))
+                    test_data_x = np.hstack((test_data_x, building_blocks_test[index]))
+                num_of_blocks += building_blocks_train.shape[0]*2
+            else:
+                data_x = np.hstack((data_x, building_blocks_train))
+                test_data_x = np.hstack((test_data_x, building_blocks_test))
+                num_of_blocks += 2
 
             print("the number of blocks is: " + str(num_of_blocks))
 
@@ -41,7 +48,7 @@ def multi_tree_gp_surrogate_model(data_x, low_dim_x, test_data_x, test_data_y, s
     else:
         use_linear_scaling = True
 
-    estimator = NSGP(pop_size=1000, max_generations=100, verbose=True, max_tree_size=100,
+    estimator = NSGP(pop_size=1000, max_generations=2, verbose=True, max_tree_size=100,
                      crossover_rate=0.8, mutation_rate=0.1, op_mutation_rate=0.1, min_depth=2,
                      initialization_max_tree_height=init_max_tree_height, tournament_size=2, use_linear_scaling=use_linear_scaling,
                      use_erc=False, use_interpretability_model=use_interpretability_model,
@@ -122,7 +129,7 @@ def gp_surrogate_model(data_x, low_dim_x, test_data_x, test_data_y, seed, use_in
 
     for index in range(num_latent_dimensions):
 
-        estimator = NSGP(pop_size=1000, max_generations=100, verbose=True, max_tree_size=100,
+        estimator = NSGP(pop_size=1000, max_generations=2, verbose=True, max_tree_size=100,
                          crossover_rate=0.8, mutation_rate=0.1, op_mutation_rate=0.1, min_depth=2,
                          initialization_max_tree_height=7, tournament_size=2, use_linear_scaling=True,
                          use_erc=False, use_interpretability_model=use_interpretability_model,
@@ -175,7 +182,7 @@ def get_building_blocks(data_x, low_dim_x, test_data_x, num_blocks, use_interpre
     else:
         use_linear_scaling = True
 
-    estimator = NSGP(pop_size=1000, max_generations=100, verbose=True, max_tree_size=5*(num_blocks+1),
+    estimator = NSGP(pop_size=1000, max_generations=2, verbose=True, max_tree_size=5*(num_blocks+1),
                      crossover_rate=0.8, mutation_rate=0.1, op_mutation_rate=0.1, min_depth=2,
                      initialization_max_tree_height=3, tournament_size=2, use_linear_scaling=use_linear_scaling,
                      use_erc=False, use_interpretability_model=use_interpretability_model,
