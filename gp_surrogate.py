ba02a1fc683a10c9280f30f8f8442c8472dd7ada
Thomas <26279201+pinouche@users.noreply.github.com>
2021-03-06 02:14:46 +0100

    add results

--- gp_surrogate.py
+++ gp_surrogate.py
@@ -46,7 +46,7 @@ def multi_tree_gp_surrogate_model(data_x, low_dim_x, test_data_x, test_data_y, s
     else:
         use_linear_scaling = False
 
-    estimator = NSGP(pop_size=1000, max_generations=2, verbose=True, max_tree_size=100,
+    estimator = NSGP(pop_size=1000, max_generations=100, verbose=True, max_tree_size=100,
                      crossover_rate=0.8, mutation_rate=0.1, op_mutation_rate=0.1, min_depth=2,
                      initialization_max_tree_height=init_max_tree_height, tournament_size=2, use_linear_scaling=use_linear_scaling,
                      use_erc=False, use_interpretability_model=use_interpretability_model,
@@ -127,7 +127,7 @@ def gp_surrogate_model(data_x, low_dim_x, test_data_x, test_data_y, seed, use_in
 
     for index in range(num_latent_dimensions):
 
-        estimator = NSGP(pop_size=1000, max_generations=2, verbose=True, max_tree_size=100,
+        estimator = NSGP(pop_size=1000, max_generations=100, verbose=True, max_tree_size=100,
                          crossover_rate=0.8, mutation_rate=0.1, op_mutation_rate=0.1, min_depth=2,
                          initialization_max_tree_height=7, tournament_size=2, use_linear_scaling=True,
                          use_erc=False, use_interpretability_model=use_interpretability_model,
@@ -180,7 +180,7 @@ def get_building_blocks(data_x, low_dim_x, test_data_x, num_blocks, use_interpre
     else:
         use_linear_scaling = False
 
-    estimator = NSGP(pop_size=1000, max_generations=2, verbose=True, max_tree_size=100,
+    estimator = NSGP(pop_size=1000, max_generations=100, verbose=True, max_tree_size=100,
                      crossover_rate=0.8, mutation_rate=0.1, op_mutation_rate=0.1, min_depth=2,
                      initialization_max_tree_height=3, tournament_size=2, use_linear_scaling=use_linear_scaling,
                      use_erc=False, use_interpretability_model=use_interpretability_model,
